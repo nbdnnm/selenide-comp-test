254fcd06e69e73315d0e096469beb6e553874a4b
eugene.kazakov <eugene.kazakov@bestseller.com>
2018-08-08 16:28:01 +0200

    selenide screenshot turned off

--- src/test/java/io/fake/BaseTest.java
+++ src/test/java/io/fake/BaseTest.java
@@ -8,8 +8,9 @@ import org.junit.BeforeClass;
 public abstract class BaseTest {
 
     @BeforeClass
-    public static void setupClass(){
+    public static void setupClass() {
         Configuration.baseUrl = "http://computer-database.gatling.io";
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.screenshots = false;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
     }
 }
